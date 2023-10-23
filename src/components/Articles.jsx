5dce469df0ed9964fccd9e33070867a1487096b2
jwiszowaty <jakubwiszowaty@hotmail.com>
2023-10-23 21:11:56 +0200

    get-comments

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -3,44 +3,58 @@ import '../styles/Articles.css'
 import axios from 'axios'
 import commentIcon from '../assets/comment-icon.png'
 import voteIcon from '../assets/vote-icon.png'
+import Comments from './Comments'
+
 function Articles() {
+
   const [articles, setArticles] = useState([])
+
   useEffect(() => {
     axios
       .get('https://be-jw-news.onrender.com/api/articles')
       .then((response) => {
         setArticles(response.data.articles)
-    })
+      })
   }, [articles])
-  
-  
+
+  const handleComments = (article_id) => {
+    setArticleId(article_id)
+  }
+
   return (
     <section id='articles-section'>
       {
         articles.map((article) => {
           return (
-            <div id='article-card'>
-              <p className='articles-topic'>{article.topic}</p>
-              <p className='articles-author'>{article.author}</p>
-              <p className='articles-date'>{article.created_at.slice(0, 10)}</p>
-              <p className='articles-time'>{article.created_at.slice(11, 19)}</p>
-              <p  className='articles-title'>{article.title}</p>
-              <div className="article-image-container">
-                <img className='article-image' src={article.article_img_url} />
+            <>
+              <div key={article.article_id} id='article-card'>
+                <p className='articles-topic'>{article.topic}</p>
+                {article.article_id}
+                <p className='articles-author'>{article.author}</p>
+                <p className='articles-date'>{article.created_at.slice(0, 10)}</p>
+                <p className='articles-time'>{article.created_at.slice(11, 19)}</p>
+                <p className='articles-title'>{article.title}</p>
+                <div className="article-image-container">
+                  <img className='article-image' src={article.article_img_url} />
+                </div>
+                <div className="comment-div" data-article_id={article.article_id} onClick={(e) => {
+                  const article_id = e.currentTarget.getAttribute('data-article_id')
+                  handleComments(article_id)
+                }}>
+                  <p className='comment-count'>{article.comment_count}</p>
+                  <img className='articles-comment-icon' src={commentIcon} />
+                </div>
+                <div className="vote-div">
+                  <p className='vote-count'>{article.votes}</p>
+                  <img className='articles-vote-icon' src={voteIcon} />
+                </div>
               </div>
-              <div className="comment-div">
-                <p className='comment-count'>{article.comment_count}</p>
-                <img className='articles-comment-icon' src={commentIcon} />
-              </div>
-              <div className="vote-div">
-                <p className='vote-count'>{article.votes}</p>
-                <img className='articles-vote-icon' src={voteIcon} />
-              </div>
-            </div>
+              <Comments />
+            </>
           )
-      })
+        })
       }
-      
+
     </section>
   )
 }
---
+++ src/components/Comments.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import '../styles/Comments.css'
+
+function Comments() {
+    
+  return (
+    <section className='comment-section'>
+      comments
+    </section>
+  )
+}
+
+export default Comments
