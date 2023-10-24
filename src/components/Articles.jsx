a88a0bfb793e07ac1c3cdb7b5a0eda5bf45f25cf
jwiszowaty <jakubwiszowaty@hotmail.com>
2023-10-24 14:50:16 +0200

    CORE: View a list of comments associated with an article

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,62 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import '../styles/Articles.css'
-import axios from 'axios'
-import commentIcon from '../assets/comment-icon.png'
-import voteIcon from '../assets/vote-icon.png'
-import Comments from './Comments'
+import NoComments from './NoComments'
+import WithComments from './WithComments'
+
 
 function Articles() {
 
   const [articles, setArticles] = useState([])
 
-  useEffect(() => {
-    axios
-      .get('https://be-jw-news.onrender.com/api/articles')
-      .then((response) => {
-        setArticles(response.data.articles)
-      })
-  }, [articles])
-
-  const handleComments = (article_id) => {
-    setArticleId(article_id)
-  }
-
-  return (
-    <section id='articles-section'>
-      {
-        articles.map((article) => {
-          return (
-            <>
-              <div key={article.article_id} id='article-card'>
-                <p className='articles-topic'>{article.topic}</p>
-                {article.article_id}
-                <p className='articles-author'>{article.author}</p>
-                <p className='articles-date'>{article.created_at.slice(0, 10)}</p>
-                <p className='articles-time'>{article.created_at.slice(11, 19)}</p>
-                <p className='articles-title'>{article.title}</p>
-                <div className="article-image-container">
-                  <img className='article-image' src={article.article_img_url} />
-                </div>
-                <div className="comment-div" data-article_id={article.article_id} onClick={(e) => {
-                  const article_id = e.currentTarget.getAttribute('data-article_id')
-                  handleComments(article_id)
-                }}>
-                  <p className='comment-count'>{article.comment_count}</p>
-                  <img className='articles-comment-icon' src={commentIcon} />
-                </div>
-                <div className="vote-div">
-                  <p className='vote-count'>{article.votes}</p>
-                  <img className='articles-vote-icon' src={voteIcon} />
-                </div>
-              </div>
-              <Comments />
-            </>
-          )
-        })
-      }
-
-    </section>
-  )
+  const [seeComments, setSeeComments] = useState([false, 0])
+    if (!seeComments[0]) {
+      return (
+        <NoComments setArticles={setArticles} articles={articles} setSeeComments={setSeeComments} />
+      )
+    } else {
+      return (
+        <WithComments setArticles={setArticles} articles={articles} setSeeComments={setSeeComments} seeComments={seeComments} />
+      )
+    }
 }
 
 export default Articles
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/Loading.css'
+function Loading() {
+  return (
+    <section className='loading'><h1>Loading . . .</h1></section>
+  )
+}
+
+export default Loading
